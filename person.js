d6a74593a1578b2833746a509ca6d14242dca113
dueaston <dueaston@gmail.com>
2023-10-19 11:40:52 -0500

    modified:   person.js Commit for private classes

--- person.js
+++ person.js
@@ -1,5 +1,12 @@
 class Person {
-  // Write code here
+  def __init__(self, name):
+        self.__id = random.randint(1000, 9999)  # Simulated private field
+        self.name = name
+
+    def get_id(self):
+        return self.__id
 }
+getter_access_id = person.get_id()
 
 module.exports = Person;
+//Not sure how to fix this 
